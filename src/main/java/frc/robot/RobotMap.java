f98cdd61349f0de09efcce532c2ed972403f8031
Victor Ghosh <vghosh@aimpa.org>
2019-01-10 16:43:40 -0500

    working on robotmap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -6,9 +6,34 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
 
 /**
  * Add your docs here.
  */
+
 public class RobotMap {
+
+     //Left Side Speed Controlers
+int leftTalonID = 16;
+int leftVictorID_1 = 22;
+int leftVictorID_2 = 21;
+//Right Side Speed Controlers
+int rightTalonID = 13;
+int rightVictorID_1 = 17;
+int rightVictorID_2 = 18;
+
+public WPI_TalonSRX leftMiniCim1, rightMiniCim1;
+public WPI_VictorSPX leftMiniCim2, leftMiniCim3, rightMiniCim2, rightMiniCim3;
+
+public DifferentialDrive drive;
+
+    public RobotMap() {
+        drive = new DifferentialDrive(PWMVictorSPX(0), PWMVictorSPX(1));
+    }
+
 }
